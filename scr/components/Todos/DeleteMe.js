886b67f8afe11cf5f2704d1323e12546ebfb13d4
Ivan <71104279+oplya@users.noreply.github.com>
2025-10-02 12:42:53 +0300

    Add files via upload

---
+++ scr/components/Todos/DeleteMe.js
@@ -0,0 +1,10 @@
+function DeleteMe() {
+  return (
+    <form>
+      <input placeholder="Enter test todo" />
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+export default DeleteMe
---
+++ scr/components/Todos/Todo.js
@@ -0,0 +1,14 @@
+import styles from './Todo.module.css'
+
+function Todo({ todo, index, deleteTodo }) {
+  return (
+    <div
+      className={styles.todo}
+      onDoubleClick={() => deleteTodo(index)}
+    >
+      <div className={styles.todoText}>{todo}</div>
+    </div>
+  )
+}
+
+export default Todo
